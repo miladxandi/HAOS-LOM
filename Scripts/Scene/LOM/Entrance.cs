42f6974152d930aabbb215e42426a65ba6e57001
Milad Xandi <info@miladzandi.ir>
2018-08-04 01:36:00 +0430

    Register Scene - MainScene - Fallout Scene

--- Scripts/Scene/LOM/Entrance.cs
+++ Scripts/Scene/LOM/Entrance.cs
@@ -11,15 +11,19 @@ namespace HAOS.Scene.LOM
         public GameObject MainCamera;
         public Image Menu;
         public Text PlayerName,StartGame;
+        public Animator MenuAnimator;
 
         // Use this for initialization
         void Start()
         {
+            MenuAnimator = MainCamera.GetComponent<Animator>();
             PlayerName.text = string.IsNullOrEmpty(PlayerPrefs.GetString("Name"))!=true ? PlayerPrefs.GetString("Name") : "ENTER YOUR NAME";
             if (PlayerPrefs.GetInt("SceneManager") == 0 || PlayerPrefs.GetInt("SceneManager") == null)
             {
                 PlayerPrefs.SetInt("SceneManager", 0);
             }
+            Menu.GetComponent<Button>().onClick.AddListener(MenuAnime);
+            PlayerName.GetComponent<Button>().onClick.AddListener(SetName);
         }
 
         // Update is called once per frame
@@ -32,8 +36,24 @@ namespace HAOS.Scene.LOM
             }
             else if (Input.GetKeyDown(KeyCode.Space) && PlayerPrefs.GetInt("SceneManager") == 1)
             {
-                SceneManager.LoadScene("MarsScene");
+                SceneManager.LoadScene("Fallout");
             }
+            
+        }
+        void MenuAnime()
+        {
+            if (MenuAnimator.GetBool("IsMenuClicked") == false)
+            {
+               MenuAnimator.SetBool("IsMenuClicked",true);
+            }
+            else
+            {
+                MenuAnimator.SetBool("IsMenuClicked", false);
+            }
+        }
+        void SetName()
+        {
+            SceneManager.LoadScene("Register");
         }
     }
 }
---
+++ Scripts/Scene/LOM/Register.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace HAOS.Scene.LOM
+{
+    public class Register : MonoBehaviour
+    {
+
+        public Button Reg,Back;
+        public InputField Name, Email, Phone;
+        // Use this for initialization
+        void Start()
+        {
+            if (PlayerPrefs.GetString("Name")!="" && PlayerPrefs.GetString("Email") != "" && PlayerPrefs.GetString("Phone") != "")
+            {
+                Name.text = PlayerPrefs.GetString("Name");
+                Email.text = PlayerPrefs.GetString("Email");
+                Phone.text = PlayerPrefs.GetString("Phone");
+            }
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            Reg.onClick.AddListener(RegisterUser);
+            Back.onClick.AddListener(BackToMain);
+        }
+        void RegisterUser()
+        {
+            if (Name.text != "" && Email.text != "" && Phone.text != "")
+            {
+                PlayerPrefs.SetString("Name", Name.text);
+                PlayerPrefs.SetString("Email", Email.text);
+                PlayerPrefs.SetString("Phone", Phone.text);
+                SceneManager.LoadScene("MainSpace");
+            }
+        }
+        void BackToMain()
+        {
+            SceneManager.LoadScene("MainSpace");
+        }
+    }
+}
